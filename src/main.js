56a853259d04e790cf11ed7b60dbc7156a87519f
Balakrishna V <balakrishnav@live.com>
2021-07-02 17:01:18 +0530

    test multiple urls

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-const Apify = require('apify');
+/* eslint-disable operator-linebreak */
+/* eslint-disable quotes */
+const Apify = require("apify");
 
 const { log, enqueueLinks } = Apify.utils;
 const { PseudoUrl } = Apify;
 
-const { basicSEO } = require('./seo.js');
-const { jsonLdLookup, microdataLookup } = require('./ontology_lookups.js');
+const { basicSEO } = require("./seo.js");
+const { jsonLdLookup, microdataLookup } = require("./ontology_lookups.js");
 
 Apify.main(async () => {
     const {
@@ -13,13 +15,18 @@ Apify.main(async () => {
         maxRequestsPerCrawl,
         maxDepth,
         seoParams,
-        userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
+        userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
         viewPortWidth,
         viewPortHeight,
         pageTimeout,
         maxRequestRetries,
         handlePageTimeoutSecs = 3600,
-    } = await Apify.getValue('INPUT');
+    } = await Apify.getValue("INPUT");
+
+    const startUrls = [
+        "https://lavu.com/",
+        "https://pizzaonline.dominos.co.in/",
+    ];
 
     log.info(`SEO audit for ${startUrl} started`);
 
@@ -29,12 +36,16 @@ Apify.main(async () => {
 
     log.info(`Web host name: ${hostname}`);
 
-    const proxyConfiguration = await Apify.createProxyConfiguration({
-        ...proxy,
-    }) || undefined;
+    const proxyConfiguration =
+        (await Apify.createProxyConfiguration({
+            ...proxy,
+        })) || undefined;
 
     const requestQueue = await Apify.openRequestQueue();
-    await requestQueue.addRequest({ url: startUrl });
+
+    startUrls.forEach(async (s) => {
+        await requestQueue.addRequest({ url: s });
+    });
 
     const crawler = new Apify.PuppeteerCrawler({
         requestQueue,
@@ -55,7 +66,7 @@ Apify.main(async () => {
             }
 
             return page.goto(request.url, {
-                waitUntil: 'networkidle2',
+                waitUntil: "networkidle2",
                 timeout: pageTimeout,
             });
         },
@@ -63,23 +74,23 @@ Apify.main(async () => {
             ignoreHTTPSErrors: true,
             args: [
                 // needed for CSP to be actually bypassed, and fetch work inside the browser
-                '--allow-running-insecure-content',
-                '--disable-web-security',
-                '--enable-features=NetworkService',
-                '--ignore-certificate-errors',
+                "--allow-running-insecure-content",
+                "--disable-web-security",
+                "--enable-features=NetworkService",
+                "--ignore-certificate-errors",
             ],
         },
         maxRequestRetries,
         maxRequestsPerCrawl,
         handlePageTimeoutSecs,
         handlePageFunction: async ({ request, page }) => {
-            log.info('Start processing', { url: request.url });
+            log.info("Start processing", { url: request.url });
 
             const data = {
                 url: page.url(),
                 title: await page.title(),
                 isLoaded: true,
-                ...await basicSEO(page, seoParams),
+                ...(await basicSEO(page, seoParams)),
                 jsonLd: await jsonLdLookup(page),
                 microdata: await microdataLookup(page),
             };
@@ -89,16 +100,17 @@ Apify.main(async () => {
             // Enqueue links, support SPAs
             const enqueueResults = await enqueueLinks({
                 page,
-                selector: 'a[href]:not([target="_blank"]),a[href]:not([rel*="nofollow"]),a[href]:not([rel*="noreferrer"])', // exclude externals
+                selector:
+                    'a[href]:not([target="_blank"]),a[href]:not([rel*="nofollow"]),a[href]:not([rel*="noreferrer"])', // exclude externals
                 pseudoUrls: [pseudoUrl],
                 requestQueue,
                 transformRequestFunction: (r) => {
                     const url = new URL(r.url);
                     url.pathname = url.pathname
-                        .split('/')
-                        .filter(s => s)
+                        .split("/")
+                        .filter((s) => s)
                         .slice(0, maxDepth)
-                        .join('/');
+                        .join("/");
 
                     return {
                         url: url.toString(),
@@ -106,10 +118,14 @@ Apify.main(async () => {
                 },
             });
 
-            const newRequests = enqueueResults.filter((result) => (!result.wasAlreadyPresent));
+            const newRequests = enqueueResults.filter(
+                (result) => !result.wasAlreadyPresent
+            );
 
             if (newRequests.length) {
-                log.info(`${request.url}: Added ${newRequests.length} urls to queue.`);
+                log.info(
+                    `${request.url}: Added ${newRequests.length} urls to queue.`
+                );
             }
 
             log.info(`${request.url}: Finished`);
